038cf8140ba30595734778bbaac20d4d4d61139b
Nick Baker <nbaker@coreweave.com>
2026-01-28 20:37:16 -0800

    add init func to lib

--- src/project-template/src/lib.rs
+++ src/project-template/src/lib.rs
@@ -8,6 +8,20 @@ pub use types::*;
 pub mod dl;
 pub use dl::*;
 
+// A function we want to run at load time of the library.
+#[no_mangle]
+pub extern "C" fn custom_init_function() {
+    eprintln!("--- RUST INIT SUCCESSFUL ---");
+}
+
+// A static reference to the initialization function pointer is placed
+// in the .init_array section using linker directives.
+#[used]
+// This attribute ensures the compiler doesn't optimize away the
+// static item if it thinks it's unused.
+#[link_section = ".init_array"]
+pub static INITIALIZER: extern "C" fn() = custom_init_function;
+
 // Generated function stubs
 {{FUNCTION_STUBS}}
 
